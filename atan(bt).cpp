70d30cfd243cf8bc46162d1aead836a611161917
TaehyeonRyu <163230402+TaehyeonRyu@users.noreply.github.com>
2024-03-21 13:33:58 +0900

    Add files via upload

---
+++ atan(bt).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+const float PI = 3.141592;
+void main() {
+	float a, b, dt, t, num, anal;
+	//differentiation
+	cout << " a b ? = ";
+	cin >> a >> b;
+	cout << " dt t ? = ";
+	cin >> t >> dt;
+	t = t * PI / 180;
+	anal = a * b * sec^2(b*t);
+
+
+}
